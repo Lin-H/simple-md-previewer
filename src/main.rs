7540411f0ce5e4197d925ede500561c311286a2b
Lin-H <lh04667649@qq.com>
2018-11-22 00:16:42 +0800

    第一次打包，修改为内嵌页面

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![windows_subsystem = "windows"]
+
 extern crate web_view;
 
 use web_view::*;
@@ -5,7 +7,7 @@ use web_view::*;
 fn main() {
     WebViewBuilder::new()
         .title("Markdown")
-        .content(Content::Url("http://localhost:1234/"))
+        .content(Content::Html(include_str!("../html/dist/bundle.html")))
         .size(800, 600)
         .resizable(true)
         .debug(true)
