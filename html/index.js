dc25b9107eeeaa656167e49d0ebc58e083ce7cd2
Lin-H <lh04667649@qq.com>
2018-11-23 20:50:53 +0800

    pick highlight grammer

--- html/index.js
+++ html/index.js
@@ -1,6 +1,22 @@
 import 'highlight.js/styles/monokai-sublime.css'
 import 'github-markdown-css'
-import hljs from 'highlight.js'
+import hljs from 'highlight.js/lib/highlight.js'
+
+hljs.registerLanguage('cpp', require('highlight.js/lib/languages/cpp'));
+hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'));
+hljs.registerLanguage('css', require('highlight.js/lib/languages/css'));
+hljs.registerLanguage('markdown', require('highlight.js/lib/languages/markdown'));
+hljs.registerLanguage('go', require('highlight.js/lib/languages/go'));
+hljs.registerLanguage('java', require('highlight.js/lib/languages/java'));
+hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
+hljs.registerLanguage('json', require('highlight.js/lib/languages/json'));
+hljs.registerLanguage('lua', require('highlight.js/lib/languages/lua'));
+hljs.registerLanguage('python', require('highlight.js/lib/languages/python'));
+hljs.registerLanguage('rust', require('highlight.js/lib/languages/rust'));
+hljs.registerLanguage('scss', require('highlight.js/lib/languages/scss'));
+hljs.registerLanguage('shell', require('highlight.js/lib/languages/shell'));
+hljs.registerLanguage('sql', require('highlight.js/lib/languages/sql'));
+hljs.registerLanguage('typescript', require('highlight.js/lib/languages/typescript'));
 
 var md = require('markdown-it')({
   html: true,
